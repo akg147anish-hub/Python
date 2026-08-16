3338d0ea89b36f7f4ff5bd3e7d887f314f95acdc
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-16 23:23:22 +0530

    Add implicit type casting examples

---
+++ 01_Basics/04_Type_Casting.py/01_Implicit_Type_Casting.py
@@ -0,0 +1,16 @@
+"""
+Topic      : Implicit Type Casting
+Module     : Python Basics - Type Casting
+Description: Demonstrates automatic type conversion in Python.
+Author     : Anish Kumar Gupta
+"""
+
+integer_number = 10
+float_number = 5.5
+
+result = integer_number + float_number
+
+print("Integer:", integer_number)
+print("Float:", float_number)
+print("Result:", result)
+print("Result Type:", type(result))
