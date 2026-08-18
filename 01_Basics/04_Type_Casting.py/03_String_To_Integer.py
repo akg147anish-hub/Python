6b9ecde2d229753813be9c09423e7a5f529e23da
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-18 22:36:00 +0530

    Add string to integer conversion

---
+++ 01_Basics/04_Type_Casting.py/03_String_To_Integer.py
@@ -0,0 +1,16 @@
+"""
+Topic      : String to Integer Conversion
+
+"""
+
+number = "50"
+
+converted_number = int(number)
+
+print("Original Value:", number)
+print("Original Type:", type(number))
+
+print("Converted Value:", converted_number)
+print("Converted Type:", type(converted_number))
+
+print("After Addition:", converted_number + 10)
