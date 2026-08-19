258d5e02da8bf506bf7fa8507b8277a40cbecbe2
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-19 23:39:19 +0530

    Add string to float conversion

---
+++ 01_Basics/04_Type_Casting.py/04_String_To_Float.py
@@ -0,0 +1,14 @@
+"""
+Topic      : String to Float Conversion
+
+"""
+
+price = "999.50"
+
+converted_price = float(price)
+
+print("Original Value:", price)
+print("Original Type:", type(price))
+
+print("Converted Value:", converted_price)
+print("Converted Type:", type(converted_price))
