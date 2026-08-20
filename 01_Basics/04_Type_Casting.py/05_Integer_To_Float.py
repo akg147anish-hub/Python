a35c1255b130fd45f5e857f1cf0194936e736b36
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-20 23:43:16 +0530

    Add integer to float conversion

---
+++ 01_Basics/04_Type_Casting.py/05_Integer_To_Float.py
@@ -0,0 +1,14 @@
+"""
+Topic      : Integer to Float Conversion
+
+"""
+
+number = 25
+
+converted_number = float(number)
+
+print("Integer Value:", number)
+print("Integer Type:", type(number))
+
+print("Float Value:", converted_number)
+print("Float Type:", type(converted_number))
