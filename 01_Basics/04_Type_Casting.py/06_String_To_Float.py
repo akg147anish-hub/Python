eaac5adabec9d1f8a3aec636c40da22ecf5ea54d
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-21 23:12:57 +0530

    Add string to float practice program

---
+++ 01_Basics/04_Type_Casting.py/06_String_To_Float.py
@@ -0,0 +1,11 @@
+"""
+Question:
+Take a decimal number as a string and convert it into a float.
+"""
+
+number = input("Enter a decimal number: ")
+
+number = float(number)
+
+print("Number:", number)
+print("Type:", type(number))
