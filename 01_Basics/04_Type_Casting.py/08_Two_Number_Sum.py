820e3dc5a49a84293e341dd98a5f4b3362de52a9
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-23 23:36:07 +0530

    Add two number sum practice program

---
+++ 01_Basics/04_Type_Casting.py/08_Two_Number_Sum.py
@@ -0,0 +1,11 @@
+"""
+Question:
+Take two numbers as input and find their sum.
+"""
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+total = num1 + num2
+
+print("Sum:", total)
