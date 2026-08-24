4febb2317356a5b30b0d0f48b25d5ea6691fcddf
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-24 22:47:34 +0530

    Add marks average practice program

---
+++ 01_Basics/04_Type_Casting.py/09_Marks_Average.py
@@ -0,0 +1,12 @@
+"""
+Question:
+Take marks of three subjects and calculate the average.
+"""
+
+maths = float(input("Enter Maths marks: "))
+physics = float(input("Enter Physics marks: "))
+programming = float(input("Enter Programming marks: "))
+
+average = (maths + physics + programming) / 3
+
+print("Average Marks:", average)
