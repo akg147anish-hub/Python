35b4d551a717375589cb69154bd92d89220acca2
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-08-29 23:22:39 +0530

    Add comparison operator examples

---
+++ 01_Basics/05_Operators/03_Comparison_Operators.py
@@ -0,0 +1,16 @@
+"""
+Topic      : Comparison Operators
+Module     : Python Basics - Operators
+Description: Demonstrates comparison between values.
+Author     : Anish Kumar Gupta
+"""
+
+a = 20
+b = 10
+
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
