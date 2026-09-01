907ce3d25411ba874edf586b6b779c2eb7f0082e
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-01 23:48:37 +0530

    Add membership operator examples

---
+++ 01_Basics/05_Operators/06_Membership_Operators.py
@@ -0,0 +1,14 @@
+"""
+Topic      : Membership Operators
+Module     : Python Basics - Operators
+Description: Demonstrates 'in' and 'not in' operators.
+Author     : Anish Kumar Gupta
+"""
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Apple" in fruits)
+print("Grapes" in fruits)
+
+print("Grapes" not in fruits)
+print("Banana" not in fruits)
