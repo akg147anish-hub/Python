c405ccc800e75f50757ae907a1d34c001c7e5515
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-04 23:44:03 +0530

    Handle multiple user input

---
+++ 01_Basics/06_Input_Output/03_Multiple_Inputs.py
@@ -0,0 +1,12 @@
+"""
+Topic: Multiple Inputs
+Description: Demonstrates taking multiple values from the user.
+"""
+
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+course = input("Enter your course: ")
+
+print("Name:", name)
+print("City:", city)
+print("Course:", course)
