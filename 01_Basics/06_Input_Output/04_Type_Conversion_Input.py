ec201124d2e884eaab203b571b74fdc8f87ec15e
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-05 23:33:38 +0530

    Handle typed user input with conversion

---
+++ 01_Basics/06_Input_Output/04_Type_Conversion_Input.py
@@ -0,0 +1,13 @@
+"""
+Topic: Type Conversion with Input
+Description: Demonstrates converting user input into different data types.
+"""
+
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("Age:", age)
+print("Age Type:", type(age))
+
+print("Height:", height)
+print("Height Type:", type(height))
