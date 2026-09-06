7531ff417cedc34c5e62363e366a8e3175d15da1
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-06 23:50:39 +0530

    Demonstrate custom output separators

---
+++ 01_Basics/06_Input_Output/05_Print_Separator.py
@@ -0,0 +1,14 @@
+"""
+Topic: print() Separator
+Description: Demonstrates the sep parameter of print().
+"""
+
+print("2026", "08", "26", sep="-")
+
+print("Python", "Java", "C++", sep=" | ")
+
+name = "Anish"
+age = 19
+course = "CSE"
+
+print(name, age, course, sep=" - ")
