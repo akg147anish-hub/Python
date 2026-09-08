201a560d226b815699d8ee769e2100d80cd04e81
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-08 23:02:09 +0530

    Add formatted output using f-strings

---
+++ 01_Basics/06_Input_Output/07_F_String.py
@@ -0,0 +1,14 @@
+"""
+Topic: F-Strings
+Description: Demonstrates formatted strings in Python.
+"""
+
+name = "Jayanti"
+age = 21
+course = "Computer Science"
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I am studying {course}.")
+
+print(f"{name} is {age} years old and studies {course}.")
