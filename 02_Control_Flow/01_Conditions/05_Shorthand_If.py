bf05e357e48ff831b9edff38732127fc3df8b33b
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-16 23:33:16 +0530

    Add shorthand conditional example

---
+++ 02_Control_Flow/01_Conditions/05_Shorthand_If.py
@@ -0,0 +1,12 @@
+"""
+Topic: Shorthand if
+Description: Demonstrates a one-line conditional statement.
+"""
+
+age = 20
+
+if age >= 18:
+    print("Adult")
+
+# Shorthand if
+age >= 18 and print("Eligible")
