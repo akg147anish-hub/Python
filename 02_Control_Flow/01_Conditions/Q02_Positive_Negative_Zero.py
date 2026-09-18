5cfdde75a0de7a8b26b3113af5e5403a64a948ab
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-18 21:45:48 +0530

    Classify number as positive negative or zero

---
+++ 02_Control_Flow/01_Conditions/Q02_Positive_Negative_Zero.py
@@ -0,0 +1,13 @@
+"""
+Question:
+Check whether a number is positive, negative or zero.
+"""
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
