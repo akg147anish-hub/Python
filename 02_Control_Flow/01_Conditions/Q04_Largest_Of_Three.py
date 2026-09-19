ae55bdc96a8ddb50791545aa43a4b7a9fcb7a211
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-20 03:36:02 +0530

    Determine largest among three numbers

---
+++ 02_Control_Flow/01_Conditions/Q04_Largest_Of_Three.py
@@ -0,0 +1,15 @@
+"""
+Question:
+Find the largest of three numbers.
+"""
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    print("Largest:", num1)
+elif num2 >= num1 and num2 >= num3:
+    print("Largest:", num2)
+else:
+    print("Largest:", num3)
