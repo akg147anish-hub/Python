684a846c6d55b80c23eb7b4dae3e5f460e35ca5a
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-22 02:42:16 +0530

    Implement leap year validation

---
+++ 02_Control_Flow/01_Conditions/Q06_Leap_Year.py
@@ -0,0 +1,15 @@
+"""
+Question:
+Check whether a year is a leap year.
+"""
+
+year = int(input("Enter a year: "))
+
+if year % 400 == 0:
+    print("Leap Year")
+elif year % 100 == 0:
+    print("Not a Leap Year")
+elif year % 4 == 0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
