2d68dae0de4a7fdafdb381bdd0f8efc1af387a2f
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-09-24 12:35:15 +0530

    Check divisibility using logical operators

---
+++ 02_Control_Flow/01_Conditions/Q08_Divisible_By_5_And_11.py
@@ -0,0 +1,11 @@
+"""
+Question:
+Check whether a number is divisible by both 5 and 11.
+"""
+
+number = int(input("Enter a number: "))
+
+if number % 5 == 0 and number % 11 == 0:
+    print("The number is divisible by both 5 and 11.")
+else:
+    print("The number is not divisible by both 5 and 11.")
